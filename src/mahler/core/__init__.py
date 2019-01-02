36d9773f015988ebb8d02caf7ab96aa33c8c5cf5
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-01-02 16:12:07 -0500

    Add configuration

--- src/mahler/core/__init__.py
+++ src/mahler/core/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 """
+import os
+
 from appdirs import AppDirs
+
+from mahler.core.utils.config import Configuration, parse_config_files
 
 from ._version import get_versions
 
-from mahler.core.utils.config import Configuration
 
 VERSIONS = get_versions()
 del get_versions
@@ -17,13 +20,24 @@ __author__ = u'Xavier Bouthillier'
 __author_short__ = u'Xavier Bouthillier'
 __author_email__ = 'xavier.bouthillier@umontreal.ca'
 __copyright__ = u'2018-2019, Xavier Bouthillier'
-__url__ = 'https://gitlab.com/bouthilx/mahler'
+__url__ = 'https://github.com/bouthilx/mahler'
 
-DIRS = AppDirs(__name__, __author_short__)
+DIRS = AppDirs('mahler', __author_short__)
 del AppDirs
 
-config = Configuration()
+DEF_CONFIG_FILES_PATHS = [
+    os.path.join(DIRS.site_data_dir, 'config.yaml.example'),
+    os.path.join(DIRS.site_config_dir, 'config.yaml'),
+    os.path.join(DIRS.user_config_dir, 'config.yaml')
+    ]
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+config = Configuration()
+config.registry = Configuration()
+config.registry.add_option(
+    'type', type=str, default=None, env_var='MAHLER_REGISTRAR_TYPE')
+
+config.scheduler = Configuration()
+config.scheduler.add_option(
+    'type', type=str, default=None, env_var='MAHLER_SCHEDULER_TYPE')
+
+parse_config_files(config, DEF_CONFIG_FILES_PATHS)
