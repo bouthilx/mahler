746b6cad751425dbc62abd6e15e682e02bdcb13e
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-03-22 15:45:19 -0400

    Add commands init, info and run to schedule

--- src/mahler/cli/schedule.py
+++ src/mahler/cli/schedule.py
@@ -7,6 +7,7 @@
    :platform: Unix
    :synopsis: Executes the trials
 """
+import argparse
 import logging
 import pkg_resources
 import pprint
@@ -24,19 +25,7 @@ def build(parser):
     """Return the parser that needs to be used for this command"""
     schedule_parser = parser.add_parser('schedule', help='schedule help')
 
-    schedule_parser.add_argument(
-        '--tags', nargs='*', help='tags to select tasks to schedule')
-
-    schedule_parser.add_argument(
-        '--container', help='container used to execute the workers')
-
-    # TODO: Should the working directory be task-specific? 
-    schedule_parser.add_argument(
-        '--working-dir', help='Working directory of the workers')
-
     load_modules_parser(schedule_parser)
-
-    schedule_parser.set_defaults(func=main)
 
 
 def load_modules_parser(main_parser):
@@ -48,8 +37,85 @@ def load_modules_parser(main_parser):
         for entry_point in pkg_resources.iter_entry_points('Scheduler')
     }
 
+    load_modules_init_parser(subparsers, plugins)
+    load_modules_run_parser(subparsers, plugins)
+    load_modules_info_parser(subparsers, plugins)
+    load_modules_submit_parser(subparsers, plugins)
+
+
+def load_modules_init_parser(subparsers, plugins):
+    """Create the subparsers for the subcommands"""
+    init_parser = subparsers.add_parser('init', help='scheduler init help')
+
+    init_parser.set_defaults(func=init)
+
+    init_subparsers = init_parser.add_subparsers(dest='init', help='sub-command help')
+
     for scheduler_name, scheduler_module in plugins.items():
-        scheduler_module.build_parser(subparsers)
+        scheduler_module.build_init_parser(init_subparsers)
+
+
+def load_modules_run_parser(subparsers, plugins):
+    """Create the subparsers for the subcommands"""
+    run_parser = subparsers.add_parser('run', help='scheduler run help')
+
+    run_parser.set_defaults(func=run)
+
+    run_subparsers = run_parser.add_subparsers(dest='run', help='sub-command help')
+
+    for scheduler_name, scheduler_module in plugins.items():
+        run_subparser = scheduler_module.build_run_parser(run_subparsers)
+        run_subparser.add_argument('commandline', nargs=argparse.REMAINDER)
+
+
+def load_modules_info_parser(subparsers, plugins):
+    """Create the subparsers for the subcommands"""
+    info_parser = subparsers.add_parser('info', help='scheduler info help')
+
+    info_parser.set_defaults(func=info)
+
+    info_subparsers = info_parser.add_subparsers(dest='info', help='sub-command help')
+
+    for scheduler_name, scheduler_module in plugins.items():
+        scheduler_module.build_info_parser(info_subparsers)
+
+
+def load_modules_submit_parser(subparsers, plugins):
+    """Create the subparsers for the subcommands"""
+    submit_parser = subparsers.add_parser('submit', help='scheduler submit help')
+
+    submit_parser.set_defaults(func=main)
+
+    submit_subparsers = submit_parser.add_subparsers(dest='submit', help='sub-command help')
+
+    for scheduler_name, scheduler_module in plugins.items():
+        parser = scheduler_module.build_submit_parser(submit_subparsers)
+        parser.add_argument(
+            '--tags', nargs='*', help='tags to select tasks to schedule')
+
+        parser.add_argument(
+            '--container', help='container used to execute the workers')
+
+        # TODO: Should the working directory be task-specific? 
+        parser.add_argument(
+            '--working-dir', help='Working directory of the workers')
+
+
+def init(args):
+    logger.debug('arguments: {}'.format(pprint.pformat(args)))
+    ressources = mahler.core.resources.build(type=args['init'], **args)
+    ressources.init()
+
+def run(args):
+    logger.debug('arguments: {}'.format(pprint.pformat(args)))
+    ressources = mahler.core.resources.build(type=args['run'], **args)
+    ressources.run(args['commandline'])
+
+
+def info(args):
+    logger.debug('arguments: {}'.format(pprint.pformat(args)))
+    ressources = mahler.core.resources.build(type=args['info'], **args)
+    print(ressources.info())
 
 
 def main(args):
@@ -58,7 +124,7 @@ def main(args):
     container = args['container']
     working_dir = args['working_dir']
     registrar = mahler.core.registrar.build()
-    ressources = mahler.core.resources.build(type=args['scheduler'], **args)
+    ressources = mahler.core.resources.build(type=args['submit'], **args)
     logger.info('Retrieving tasks')
     task_documents = registrar.retrieve_tasks(
         tags=tags, container=container, status=mahler.core.status.Queued(''),
